287e78b37432fa1004b1c80ff2e15dcafd0a979c
kevinwairi <254wairi@gmail.com>
2018-09-22 08:16:02 +0300

    push to remote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'giphySearch';
-}
+  title = 'Welcome to GiphySearch';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+    http: Http;
+
+   constructor(http: Http) {
+        this.http = http;
+    }
+
+
+
+
+  performSearch(searchTerm:HTMLInputElement): void{
+	     var apiLink = this.link + searchTerm.value;
+
+        this.http.request(apiLink)
+            .subscribe((res: Response) => {
+                  this.giphies = res.json().data;
+                  console.log(this.giphies);
+            });
+                  
+	  }
+	}
